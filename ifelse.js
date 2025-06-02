a6e36cc4dcdfe28212dd6d5f6a9e8f6b8d55a45f
Anandajaiganesh <anandajaiganesh98@gmail.com>
2025-06-02 16:40:48 +0530

    changes made in branch2

---
+++ ifelse.js
@@ -0,0 +1,15 @@
+//else if
+
+let e=500
+let f= 400
+if (e==f){ console.log("e and f are equal");
+}
+else if (e===f){console.log("e and f not are equal");
+}
+else if (e<f) {console.log("e is less than f");
+}
+else if (e>f){ console.log("e is greater than f");
+}
+else if (e<=f){console.log("e less than or equal to f");
+}
+console.log("branch2 test");
