36bc8abf13e15cab367674fc06532907154e16f0
Anandajaiganesh <anandajaiganesh98@gmail.com>
2025-06-02 15:49:12 +0530

    new folders added

---
+++ condition.js
@@ -0,0 +1,34 @@
+//if condition
+
+let a= 100
+let b= 100
+if (a==b){
+    //it executes only true condition
+    console.log("Both are same");  
+}
+
+// if else- else appplies only if condition is false
+
+let c= 200
+let d= 300
+if(c==d){
+    console.log("Both are same");  
+}
+else { console.log("Both are not same");
+}
+
+
+//else if
+
+let e=500
+let f= 400
+if (e==f){ console.log("e and f are equal");
+}
+else if (e===f){console.log("e and f not are equal");
+}
+else if (e<f) {console.log("e is less than f");
+}
+else if (e>f){ console.log("e is greater than f");
+}
+else if (e<=f){console.log("e less than or equal to f");
+}
